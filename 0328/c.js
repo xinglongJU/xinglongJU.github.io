d3dd8c5cce0ab1bd01cb00a2c7994b2b20597892
xinglongJU <xinglong.ju@ou.edu>
2024-03-28 21:09:54 -0500

    Update c.js

--- 0328/c.js
+++ 0328/c.js
@@ -4,64 +4,61 @@ let R = null;
 let chart = null;// for char object
 let tbl2 = null;
 
-let b1 = document.querySelector( "#b1" );
+let b1 = document.querySelector("#b1");
 
-async function sc ()
-{
+async function sc() {
     let url = `https://juxinglong.github.io/static/data/states.json`;
 
-    let r = await fetch( url );// async
+    let r = await fetch(url);// async
     let rj = await r.json();//
 
     // get chart container
-    let cc = document.querySelector( "#cc" );
+    let cc = document.querySelector("#cc");
 
     let opts = {
         type: "pie",// line,bar,pie
         data: {
-            labels:rj.map(x=>x.st),
-            datasets: [ {
-                data: rj.map( x => x.p ),
-                label:"Population",
-            }, ],
+            labels: rj.map(x => x.st),
+            datasets: [{
+                data: rj.map(x => x.p),
+                label: "Population",
+            },],
         },
     };
 
-    if ( chart != null )
-    {
+    if (chart != null) {
         chart.destroy();
     }
     cc.innerHTML = ``;
 
-    chart = new Chart( cc, opts );
+    chart = new Chart(cc, opts);
 
     // show table
     // rj
-    let sdiv = document.querySelector( "#sdiv" );
+    let sdiv = document.querySelector("#sdiv");
 
     let p = {
         data: rj,
-        pagination: { limit: 5, },
+        pagination: {limit: 5,},
         sort: true,
         search: true,
-        columns: [ { id: "st", name: "STATE" }, {id:"p",name:"POP"}, ],
+        columns: [{id: "st", name: "STATE"}, {id: "p", name: "POP"},],
     };
 
-    if ( tbl2!=null )
-    {
+    if (tbl2 != null) {
         tbl2.destroy();
     }
     sdiv.innerHTML = ``;
 
-    tbl2 = new gridjs.Grid( p ); 
+    tbl2 = new gridjs.Grid(p);
     tbl2.render(sdiv);
 
 
-    console.log( rj );
+    console.log(rj);
     R = rj;
 
     // fetch()
     ///Swal.fire( "OK" );
 }
 
-b1.addEventListener("click",sc);
+b1.addEventListener("click", sc);
